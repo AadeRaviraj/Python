c67bd124d568f62d26b1d772b10833c0793fc229
Raviraj <ravirajaade299@gmail.com>
2026-01-29 16:29:54 +0530

    Update

---
+++ Assignments/Assignment_15/Assignment15_2.py
@@ -0,0 +1,14 @@
+# 2: Write a Python program to implement a class named BankAccount with the following requirements:
+# The class should contain two instance variables:
+# 0
+# Name (Account holder name)
+# Amount (Account balance)
+# The class should contain one class variable:
+# 0 ROI (Rate of Interest), initialized to 10.5
+# Define a constructor (_init ) that accepts Name and initial Amount.
+# Implement the following instance methods:
+# Display()- displays account holder name and current balance
+# Deposit() accepts an amount from the user and adds it to balance
+# 0 Withdraw() accepts an amount from the user and subtracts it from balance (Ensure withdrawal is allowed only if sufficient balance exists)
+# CalculateInterest() calculates and returns interest using formula: Interest = (Amount ROI) / 100
+# Create multiple objects and demonstrate all methods.
---
+++ Assignments/Assignment_15/Assignment15_3.py
@@ -0,0 +1,11 @@
+# 3: Write a Python program to implement a class named Numbers with the following specifications:
+# The class should contain one instance variable:
+# Value
+# Define a constructor (init) that accepts a number from the user and initializes Value.
+# Implement the following instance methods:
+# ChkPrime() returns True if the number is prime, otherwise returns False
+# ChkPerfect() returns True if the number is perfect, otherwise returns False
+# 0 Factors() displays all factors of the number
+# SumFactors() returns the sum of all factors
+# (You may use this method as a helper in ChkPerfect() if required)
+# Create multiple objects and call all methods.
